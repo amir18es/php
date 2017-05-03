05b93b25f260b7037cdd925e127da9efd1b7e690
amir18es <amirhoseinfo18@gmail.com>
2017-05-03 15:11:37 +0430

    Update launch.php

--- launch.php
+++ launch.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'Elemental.php';
 
-define('BOT_TOKEN', 'Token');
+define('BOT_TOKEN', '369785442:AAGc0ml6fnvjcW2cZIk0ZjNNFnwTP5HYwSo');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
